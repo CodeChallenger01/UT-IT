d1544a8c9f2a9ca16ed025d09832a738cf178b06
Manish Mishra <manish.mishra@knoldus.com>
2023-04-25 18:42:19 +0530

    Added Email Validator class

--- second-module/src/main/scala/com/knoldus/validator/EmailValidator.scala
+++ second-module/src/main/scala/com/knoldus/validator/EmailValidator.scala
@@ -2,15 +2,28 @@ package com.knoldus.validator
 
 import com.typesafe.scalalogging.Logger
 
+import scala.util.{Failure, Success, Try}
+
 class EmailValidator {
   val loggers = Logger(getClass)
 
-  def emailIdIsValid(emailId: String): Boolean = {
-    loggers.info("Inside the emailIdIsValid method to check email id is valid or not")
-    emailId match {
-      case "knoldus@gmail.com" => true
-      case "philips123@gmail.com" => true
-      case _ => false
+  def emailIdIsValid(userEmail: String): Boolean = {
+    Try {
+      loggers.info("Verifying the email id is valid or not")
+      val listOfSpecialCharacter = List('!', '#', '$', '^', '&', '*', '(', ')', '~', '+', '-', '/')
+      val isEmailContainingSpecialCharacter = !(userEmail.count(specialCharacter => listOfSpecialCharacter.contains(specialCharacter)) >= 1)
+      userEmail match {
+        case email if email.count(_ == '@') == 1 && email.contains(".com") && isEmailContainingSpecialCharacter && email.length < 30 =>
+          val substring = email.substring(email.indexOf('@') + 1, email.lastIndexOf('.')).length
+          val isUserNameBeforeAtTheRate = email.substring(0, email.indexOf('@')).length
+          if (substring > 2 && isUserNameBeforeAtTheRate > 2) true
+          else throw new RuntimeException("less character between @ and . OR email doesn't contain user name")
+
+        case _ => throw new RuntimeException("Email is not Appropriate")
+      }
+    } match {
+      case Success(_) => true
+      case Failure(_) => false
     }
   }
 }

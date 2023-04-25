831436f20155c99dcac40f9c6f2143f79b847e71
Manish Mishra <manish.mishra@knoldus.com>
2023-04-25 17:50:52 +0530

    Added test cases of company validator

---
+++ second-module/src/test/scala/com/knoldus/validator/CompanyValidatorTest.scala
@@ -0,0 +1,66 @@
+package com.knoldus.validator
+
+import com.knoldus.models.Company
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+
+class CompanyValidatorTest extends AnyFlatSpec {
+
+  val companyValidator = new CompanyValidator
+
+  "companyIsValid" should "return true if company exists" in {
+    val company = Company("Knoldus", "knoldus@gmail.com", "Noida")
+    val actualOutput = companyValidator.companyIsValid(company)
+    val expectedOutput = true
+    expectedOutput shouldBe actualOutput
+  }
+
+  "companyIsValid" should "not return true if company not exists" in {
+    val company = Company("Google", "google@gmail.com", "Noida")
+    val actualOutput = companyValidator.companyIsValid(company)
+    val expectedOutput = true
+    assert(actualOutput != expectedOutput)
+  }
+
+  "companyIsValid" should "return false if company not exists" in {
+    val company = Company("Google", "google@gmail.com", "Noida")
+    val actualOutput = companyValidator.companyIsValid(company)
+    val expectedOutput = false
+    expectedOutput shouldBe actualOutput
+  }
+
+  "companyIsValid" should "not return false if company not exists" in {
+    val company = Company("Google", "google@gmail.com", "Noida")
+    val actualOutput = companyValidator.companyIsValid(company)
+    val expectedOutput = true
+    assert(expectedOutput != actualOutput)
+  }
+
+  "companyIsValid" should "return true if other company exists" in {
+    val company = Company("Philips", "philips123@gmail.com", "Noida")
+    val actualOutput = companyValidator.companyIsValid(company)
+    val expectedOutput = true
+    expectedOutput shouldBe actualOutput
+  }
+
+  "companyIsValid" should "not return true company name if other company not exists" in {
+    val company = Company("Google", "philips123@gmail.com", "Noida")
+    val actualOutput = companyValidator.companyIsValid(company)
+    val expectedOutput = true
+    assert(actualOutput != expectedOutput)
+  }
+  "companyIsValid" should "return false if email id is not verified" in {
+    val company = Company("Philips", "google@gmail.com", "Noida")
+    val actualOutput = companyValidator.companyIsValid(company)
+    val expectedOutput = false
+    expectedOutput shouldBe actualOutput
+  }
+
+  "companyIsValid" should "not return false if email id verified" in {
+    val company = Company("Google", "philips123@gmail.com", "Noida")
+    val actualOutput = companyValidator.companyIsValid(company)
+    val expectedOutput = true
+    assert(actualOutput != expectedOutput)
+  }
+}
+

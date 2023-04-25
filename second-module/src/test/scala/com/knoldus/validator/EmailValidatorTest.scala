3b6e646f60d7c2e99da0bf494178f1ae0a059840
Manish Mishra <manish.mishra@knoldus.com>
2023-04-25 17:51:09 +0530

    Added test cases of email validator

---
+++ second-module/src/test/scala/com/knoldus/validator/EmailValidatorTest.scala
@@ -0,0 +1,66 @@
+package com.knoldus.validator
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class EmailValidatorTest extends AnyFlatSpec with Matchers {
+
+  val emailValidator = new EmailValidator
+
+  "emailIdIsValid" should "return false if invalid mail id passed" in {
+    val emailId = ""
+    val actualOutput = emailValidator.emailIdIsValid(emailId)
+    val expectedOutput = false
+    expectedOutput shouldBe actualOutput
+  }
+
+  "emailIdIsValid" should "not return true if invalid mail id passed" in {
+    val emailId = ""
+    val actualOutput = emailValidator.emailIdIsValid(emailId)
+    val expectedOutput = true
+    assert(expectedOutput != actualOutput)
+  }
+
+  "emailIdIsValid" should "return false if mail id not in db" in {
+    val emailId = "google@gmail.com"
+    val actualOutput = emailValidator.emailIdIsValid(emailId)
+    val expectedOutput = false
+    expectedOutput shouldBe actualOutput
+  }
+
+  "emailIdIsValid" should "not return true if mail id not in db" in {
+    val emailId = "google@gmail.com"
+    val actualOutput = emailValidator.emailIdIsValid(emailId)
+    val expectedOutput = true
+    assert(expectedOutput != actualOutput)
+  }
+
+  "emailIdIsValid" should "return true if mail id in db" in {
+    val emailId = "knoldus@gmail.com"
+    val actualOutput = emailValidator.emailIdIsValid(emailId)
+    val expectedOutput = true
+    expectedOutput shouldBe actualOutput
+  }
+
+  "emailIdIsValid" should "not return false if mail id in db" in {
+    val emailId = "knoldus@gmail.com"
+    val actualOutput = emailValidator.emailIdIsValid(emailId)
+    val expectedOutput = false
+    assert(expectedOutput != actualOutput)
+  }
+
+  "emailIdIsValid" should "return true if other mail id in db" in {
+    val emailId = "philips123@gmail.com"
+    val actualOutput = emailValidator.emailIdIsValid(emailId)
+    val expectedOutput = true
+    expectedOutput shouldBe actualOutput
+  }
+
+  "emailIdIsValid" should "not return true if mail id in db" in {
+    val emailId = "philips123@gmail.com"
+    val actualOutput = emailValidator.emailIdIsValid(emailId)
+    val expectedOutput = false
+    assert(expectedOutput != actualOutput)
+  }
+
+}
